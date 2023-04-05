36e870b6adf88d76ad89296bc4517a1d8bd15600
Inda Smith <inda.k.smith@gmail.com>
2023-04-05 05:07:13 -0400

    still a lot of work to do, too tired to think

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your code here...
+const baseUrl = 'http://localhost:3000/'
+const menuUrl = 'http://localhost:3000/menu'
+
+function fetchMenu() {
+    fetch(menuUrl)
+    .then(response => response.json)
+    .then(menuData => renderMenu(menuData))
+}
+fetchMenu()
+
+
+const renderMenu = menu => {
+    renderMenuItem(menu[0])
+    const menuItemsDiv = document.getElementById('menu-items')
+    const menuItems = document.createElement('span')
+    menuItemsDiv.appendChild(menuItems)
+    
+    menu.forEach(recipe => {
+        const recipename
+    })
+    menuItems.textContent = `${menu.name}`
+}
+
+
+
+
+
+
+
+
+
+//     const menuItemSpan = document.createElement('span')
+//         menuItemsDiv.appendChild(menuItemSpan)
+
+//         menuItemSpan.textContent = `${game.name} (${game.manufacturer_name})`
+
+//         gameNameH5.onclick = () => {
+//             renderGameDetails(game)
